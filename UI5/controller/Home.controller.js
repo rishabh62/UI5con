1f1112803c88133df74358feed33ba601bf8917b
Rishabh Gour <rishabh.gour@evorait.com>
2019-06-12 23:43:55 +0530

    added couple of controls in view for demo and removed previous code from controller

--- UI5/controller/Home.controller.js
+++ UI5/controller/Home.controller.js
@@ -7,19 +7,5 @@ sap.ui.define([
 
 	return Controller.extend("ui5con.controller.Home", {
 
-        onInit: function(){
-            var oData = {
-                count: 0
-            };
-            var counterModel = new JSONModel(oData);
-            this.getView().setModel(counterModel, "counterModel");
-        },
-
-        onClick: function(oEvent){
-            var counterModel = this.getView().getModel("counterModel");
-            var count = counterModel.getProperty("/count");
-            this.getView().getModel("counterModel").setProperty("/",{count: count + 1});
-        }
-
 	});
 });
