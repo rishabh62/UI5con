2da7c27c2c4daedf75831391a346f00e5fbff48f
Rishabh Gour <rishabh.gour@evorait.com>
2019-06-16 02:16:42 +0530

    added table with rows

---
+++ UI5 webcomponents/main.js
@@ -0,0 +1,13 @@
+import "@ui5/webcomponents/dist/ShellBar";
+import "@ui5/webcomponents/dist/Input";
+import "@ui5/webcomponents/dist/ShellBarItem";
+import "@ui5/webcomponents/dist/TabContainer"; 
+import "@ui5/webcomponents/dist/Tab";  
+import "@ui5/webcomponents/dist/TabSeparator"; 
+import "@ui5/webcomponents/dist/Table";
+import "@ui5/webcomponents/dist/TableColumn";
+import "@ui5/webcomponents/dist/TableRow";
+import "@ui5/webcomponents/dist/TableCell";
+import "@ui5/webcomponents/dist/Label";
+import "@ui5/webcomponents/dist/Button";
+
