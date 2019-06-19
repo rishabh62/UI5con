8597cebf27d80d82a1cf23cd6e90f893d1a71ee0
Rishabh Gour <rishabh.gour@evorait.com>
2019-06-19 16:01:01 +0530

    removed hardcoded data and now fetchning from northwind service

--- UI5/controller/Home.controller.js
+++ UI5/controller/Home.controller.js
@@ -7,40 +7,35 @@ sap.ui.define([
 
 	return Controller.extend("ui5con.controller.Home", {
 
-		// onInit: function(){
-		// 	this.count = 4;
-		// 	this.url = "https://services.odata.org/V2/Northwind/Northwind.svc/Products({count})?$select=ProductName,QuantityPerUnit,UnitPrice,UnitsInStock&$format=json";
-		// },
+		onInit: function () {
+			this.count = 4;
+			this.url = "https://services.odata.org/V2/Northwind/Northwind.svc/Products({count})?$select=ProductName,QuantityPerUnit,UnitPrice,UnitsInStock&$format=json";
+		},
 
 		onRefresh: function (oEvent) {
-			// var url = this.url.replace("{count}", this.count++);
+			var url = this.url.replace("{count}", this.count++);
+			this.getView().byId("table").setBusy(true);
+			$.ajax({
+				url: url,
+				type: "GET",
+				contentType: "application/json",
+				url: url,
+				async: true,
+				success: function (result) {
+					var table = this.getView().byId("table");
+					var oNewTableRow = new sap.m.ColumnListItem();
+					oNewTableRow.addCell(new sap.m.Text({ text: result.d.ProductName }));
+					oNewTableRow.addCell(new sap.m.Text({ text: result.d.QuantityPerUnit }));
+					oNewTableRow.addCell(new sap.m.Text({ text: "$" + (+result.d.UnitPrice).toFixed(2) }));
+					oNewTableRow.addCell(new sap.m.Text({ text: result.d.UnitsInStock }));
+					table.addItem(oNewTableRow);
+					table.setBusy(false);
+				}.bind(this),
+				error: function () {
+					console.log("Something went wrong while fetching the data!");
+				}
+			});
 
-			// $.ajax({url: url,
-			// 	type: "GET",
-			// 	contentType: "application/json",
-			// 	url: "https://services.odata.org/V2/Northwind/Northwind.svc/Products?$format=json",
-			// 	dataType: "jsonp",
-			// 	async: false,
-			// 	success: function(result){
-			// 		debugger;
-			// 	},
-			// 	error: function(){
-			// 		debugger;
-			// 	}
-			// });
-			
-
-			var table = this.getView().byId("table");
-			var oNewTableRow = new sap.m.ColumnListItem();
-			oNewTableRow.addCell(new sap.m.Text({ text: "Chef Anton's Cajun Seasoning" }));
-			oNewTableRow.addCell(new sap.m.Text({ text: "48 - 6 oz jars" }));
-			oNewTableRow.addCell(new sap.m.Text({ text: "$22.00" }));
-			oNewTableRow.addCell(new sap.m.Text({ text: "53" }));
-			table.setBusy(true);
-			setTimeout(function () { 
-				table.addItem(oNewTableRow);
-				table.setBusy(false);
-			}, 2000);
 		}
 
 	});
